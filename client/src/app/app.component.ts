228cfffa5247fe3c71627ccac92f13666f6c8322
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-06-19 21:35:25 +0300

    Section 3: Connecting client and API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Dabble App';
+  user: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:7183/api/users').subscribe({
+      next: response => this.user = response,
+      error: error => console.log(error),
+      complete: () => console.log("Request has completed")
+    });
+  }
 }
