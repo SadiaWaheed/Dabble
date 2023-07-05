b55a2b0a9227db6c88e3e4b472664ef9fc91164c
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-07-05 12:06:06 +0300

    Section 8: Seeding Data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +24,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//order is important here.
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetService<ILogger>();
+    logger.LogError(ex, "An error occured during migration.");
+}
 
 app.Run();
