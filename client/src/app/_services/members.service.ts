64f64e1778ca7479cdaa7dd8c07a0ceba7733905
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-07-12 21:28:49 +0300

    Section 10: Updating the user in the client app

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/members';
+import { publishFacade } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,8 @@ export class MembersService {
 
   getMember(username: string) {
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl+'users', member);
   }
 }
