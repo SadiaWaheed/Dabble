ab1fdef0362fa807db1de1676370cb178804b1f2
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-07-20 14:18:53 +0300

    Section 13: Adding additional filters

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -12,5 +12,7 @@ namespace API.Helpers
         }
         public string CurrentUserName { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 100;
     }
 }
