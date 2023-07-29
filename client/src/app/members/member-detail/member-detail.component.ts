ac4b21b3e6f1f20a46dc83dcf9c1337a545ddc6a
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-07-29 14:58:33 +0300

    interceptors folder fix

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -69,7 +69,7 @@ export class MemberDetailComponent implements OnInit {
 
   selectTab(heading: string){
     if(this.memberTabs){
-      this.memberTabs!.tabs.find(x => x.heading === heading)!.active = true; 
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true; 
     }        
   }
 
