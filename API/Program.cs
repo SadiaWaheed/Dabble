9c6eaed27aaade108835351bf03c3505e7f054ea
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2023-08-06 22:26:56 +0300

    Section 17: Adding a presence hub

--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using API.Extensions;
 using API.Middleware;
+using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,6 +27,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<PresenceHub>("hubs/presence");
 
 //order is important here.
 using var scope = app.Services.CreateScope();
